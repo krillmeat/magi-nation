1251def66102047bf41e5dc9bba1dc2eda7769dc
Ross Conover <conover.ross@gmail.com>
2024-03-09 16:00:37 -0500

    structural updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Filter from './filter/filter';
+import CardLibrary from './library/card-library';
+import TopNavigation from './navigation/top-navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <TopNavigation />
+      <Filter />
+      <CardLibrary />
     </div>
   );
 }
---
+++ src/navigation/top-navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './top-navigation.css';
+
+const TopNavigation = () => {
+  return <nav className="top">
+    <div className='bar'>
+      <h1>Magi Nation</h1>
+      <button className='hamb'>...</button>
+    </div>
+  </nav>
+}
+
+export default TopNavigation;
